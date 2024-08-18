1b0db4e623bdb9efc32b21ca72b3020beac58f29
Golery <golery.team@golery.com>
2024-08-18 12:21:54 -0400

    dev

--- src/middlewares/AuthMiddleware.ts
+++ src/middlewares/AuthMiddleware.ts
@@ -1,10 +1,12 @@
 import express from 'express';
 import {MOCK_TOKEN} from '../services/AccountService';
+import { APP_ID_HEADER } from '../contants';
 
 export const authMiddleware = (req: express.Request, res: express.Response, next: express.NextFunction) => {
     const authorization = req.header('Authorization');
     if (authorization === `Bearer ${MOCK_TOKEN}`) {
-        Object.assign(req, { ctx: { userId: 1, appId: 1}});
+        const appId = req.header(APP_ID_HEADER);
+        Object.assign(req, { ctx: { userId: 1, appId}});
         next();
     } else {
         console.log('No auth');
