f7c9b934221db7d944ef7a1c81fc93e15ae234e0
golery <golery.team@gmail.com>
2026-01-18 17:07:49 -0500

    releease script

--- scripts/release.sh
+++ scripts/release.sh
@@ -5,7 +5,8 @@ TIMESTAMP=$(date +'%Y%m%d%H%M%S')
 TAG=$TIMESTAMP
 docker tag golery/goapi:sandbox golery/goapi:$TAG
 docker push golery/goapi:$TAG
-echo
+
 echo Pushed image golery/goapi:$TAG
-echo Run this command in GCP: TAG=$TAG source /home/lyhoanghai/app-configs/scripts/run-goapi.sh
-echo RUN this command in GCP: docker logs --follow goapi
+echo "Executing: gcloud compute ssh --project=golery --zone=us-central1-c lyhoanghai@goapi-1 --command=\"TAG=$TAG source /home/lyhoanghai/app-configs/scripts/run-goapi.sh\""
+gcloud compute ssh --project=golery --zone=us-central1-c lyhoanghai@goapi-1 --command="TAG=$TAG source /home/lyhoanghai/app-configs/scripts/run-goapi.sh"
+gcloud compute ssh --project=golery --zone=us-central1-c lyhoanghai@goapi-1 --command='docker logs --follow goapi'
